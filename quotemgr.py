cfc15316a041decf1fc9ecb8e9cadc52d0cb9b2e
Kalobi <46748261+Kalobi@users.noreply.github.com>
2020-10-21 18:54:58 +0200

    Fetching all quotes

--- quotemgr.py
+++ quotemgr.py
@@ -1,11 +1,18 @@
 import sys
+import itertools as it
 import requests
 
 
 def fetch_quote(token, number):
     r = requests.get("https://twitch.center/customapi/quote", params={"token": token, "data": number})
-    print(r)
+    return r.text
+
+
+def fetch_all_quotes(token):
+    return list(it.takewhile(lambda quote: quote[0].isdigit(),
+                             (fetch_quote(token, num) for num in it.count(1))))
 
 
 if __name__ == "__main__":
-    fetch_quote(sys.argv[1], 33)
+    for quote in fetch_all_quotes(sys.argv[1]):
+        print(quote)
